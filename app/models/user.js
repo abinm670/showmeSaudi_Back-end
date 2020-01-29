9d924b4cefb09dfbce5a1d9876c9c90a59a8e290
Abdullah Binmahfouz <abinm670@mtroyal.ca>
2020-01-29 16:40:02 +0300

    complete the database redesign and the create the comment

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,6 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-
 //Comments Schema 
 var commentsSchema = new Schema(
   {
@@ -13,71 +12,75 @@ var commentsSchema = new Schema(
 
 )
 
-// pointSchema
-const pointSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    enum: ['Point'],
-    required: true
-  },
-  coordinates: {
-    type: [Number],
-    required: true
-  }
-});
-
-//Touring Schema
-var touringSchema = new Schema(
-  {
-    AboutMe: { type: String, required: [true, "can't be blank"] },
-    activity: [],
-    likes: { type: Number, default: 0 },
-    comments: { type: Schema.Types.ObjectId, ref: "comment"},
-    // comments:[],
-   
-
-
-// check this latter     
-      tourGuyImg: {type:String, required: [true, "should upload image"]},  
- 
-        comments:{type : Schema.Types.ObjectId , ref: "comment"}
-
-    // check this latter     
-    //img: {type:String, required: [true, "should upload image"]},  
-
-
-
-
-
-
-    // will test this latter
-    //location: {
-    //     cityName:String, 
-    //     type: pointSchema,
-    //     required: true
-    //   } 
-    // you might have agency as a user in future.
-    //         sponsored:{type:Boolean, default:false},     
-  }, { timestamps: true }
-)
 var userSchema = new Schema(
   {
     email: { type: String, lowercase: true, unique: [true, "this email taken"], required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'] },
     password: { type: String, required: [true, "can't be blank"] },
     firstName: { type: String, required: [true, "can't be blank"] },
     lastName: { type: String, required: [true, "can't be blank"] },
-    tour: { type: Boolean, default: false },
+    tourType: { type: String, default: "regUser" },
     address: String,
     phone: String,
-    touring: [touringSchema],
+    // img: {type:String, required: [true, "should upload image"]},
 
-  }
-  , { timestamps: true }
-)
+    // second user 
+    comments: [{ type: Schema.Types.ObjectId, ref: "comment" }],
+    tourGuy: {
+      AboutMe: { type: String },
+      package: [{
+        packName: { type: String },
+        // {packImg: {type:String, required: [true, "should upload image"]}},
+        description: { type: String },
+      }],
+      likes: { type: Number, default: 0 },
+
+      price: { type: String, default: "free" }
+    },
+    //  {timestamps: true }
+  })
+
+
 //manipulate data with Models 
 var User = mongoose.model("User", userSchema);
-var Touring = mongoose.model("Touring", touringSchema);
 var Comment = mongoose.model("Comment", commentsSchema);
 
 //Export Models
-module.exports = {User, Touring,Comment}
+module.exports = { User, Comment }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for latter  google map 
+
+  //   // will test this latter
+  //   //location: {
+  //   //     cityName:String, 
+  //   //     type: pointSchema,
+  //   //     required: true
+  //   //   } 
+  //   // you might have agency as a user in future.
+  //   //         sponsored:{type:Boolean, default:false},     
+  // }, { timestamps: true }
+
+
+  // pointSchema
+// const pointSchema = new mongoose.Schema({
+//   type: {
+//     type: String,
+//     enum: ['Point'],
+//     required: true
+//   },
+//   coordinates: {
+//     type: [Number],
+//     required: true
+//   }
+// });
--- app/routes/users.js
+++ app/routes/users.js
@@ -5,37 +5,14 @@ var mongoose = require("mongoose");
 const router = express.Router();
 ///for image
 const multer = require('multer');
+const middlewares = require('./middlewares');
 
-const storage = multer.diskStorage({
-    //cb call back function
-  destination: function (req,file,cb){
-    // make uploads folder to store img
-    cb(null, './uploads/');
-  },
-  filename: function(req,file,cb){
-    cb(null,  Date.now()+file.originalname);
-  }
-});
+const path = require('path');
 
-const fileFilter=(req,file,cb)=>{
-  //file not accept if this false
-  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-    //accept
-    cb(null,true);
-  }
-  else{
-    //reject
-    cb(null,false);
-  }
-}
 
-const upload = multer({
-  storage:storage,
-  limits:{
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter:fileFilter
-})
+
+
+
 
 // fixe the DeprecationWarning:
 mongoose.set('useNewUrlParser', true);
@@ -55,7 +32,6 @@ passport.use(strategy);
 
 // Require Mongoose Model for User & Touring
 var User = require('../models/user').User
-var Touring = require('../models/user').Touring
 var Comment = require('../models/user').Comment
 
 // Middleware required for post
@@ -63,179 +39,170 @@ var Comment = require('../models/user').Comment
 
 
 //create user 
-router.post('/api/newUser', upload.single('tourGuyImg'), (req, res) => {
-    // if(req.body.touring[1])
-    // {
-    //store new Touring profile with data from request body
-    // var newTourProfile = new Touring({ newTourProfile: req.body.newTourProfile });
-    if (req.body.touring !== undefined) {
-      newuser = req.body
-      newuser.tour = true;
-      newuser.tourGuyImg=req.file.path
-     
-    User.create(newuser)
-    .then  (newTuser=> {
-      res.json(newTuser);
-        
-    }).catch((err) =>
-    {
-        console.log("tour user cant be created",  err);
+router.post('/api/newUser', middlewares.upload.single('tourGuyImg'), (req, res) => {
+  // if(req.body.touring[1])
+  // {
+  //store new Touring profile with data from request body
+  // var newTourProfile = new Touring({ newTourProfile: req.body.newTourProfile });
+  if (req.body.tourGuy !== undefined) {
+    newuser = req.body
+    newuser.tourType = "tourUser";
+    // newuser.tourGuyImg = req.file.path
 
-    });
-  }else {
+    User.create(newuser)
+      .then(newTuser => {
+        res.json(newTuser);
+
+      }).catch((err) => {
+        console.log("tour user cant be created", err);
+
+      });
+  } else {
     User.create(req.body)
-    .then (newRuser =>
-      {
+      .then(newRuser => {
         res.json(newRuser);
-      }).catch((err)=> console.log("regular user cant be created", err))
+      }).catch((err) => console.log("regular user cant be created", err))
   }
 });
 
 // show specific user 
 router.get('/api/user/:id', (req, res) => {
-    User.findById(req.params.id, (err, foundUser) => {
-        res.send(foundUser)
-    })
+  User.findById(req.params.id, (err, foundUser) => {
+    res.send(foundUser)
+  })
 })
 
 //show all user
 router.get('/api/users', (req, res) => {
 
-    User.find()
-        .then(user => {
-            res.send(user)
-            console.log("okay")
+  User.find()
+    .then(user => {
+      res.send(user)
+      console.log("okay")
 
-        }).catch(err => console.log(err))
-    // User.find({}, (err, foundUser) => {
-    //     res.send(foundUser);
+    }).catch(err => console.log(err))
+  // User.find({}, (err, foundUser) => {
+  //     res.send(foundUser);
 
-    // })
+  // })
 })
 
 // delete user account
 router.delete('/api/user/delete/:id', (req, res) => {
-    User.findByIdAndRemove(req.params.id, (err, data) => {
-        if (err) {
-            console.log("user not delete", err)
-        }
-        else {
-            res.redirect('/api/users');
-            console.log("deleted perfect")
-            
-        }
-    })
+  User.findByIdAndRemove(req.params.id, (err, data) => {
+    if (err) {
+      console.log("user not delete", err)
+    }
+    else {
+      res.redirect('/api/users');
+      console.log("deleted perfect")
+
+    }
+  })
 })
 
 //Update tour guy profile 
 // router.put('/api/user_account/:u_id/profile/:id', (req, res) => {
-    // set a new value of the user and profile 
-    // var u_id = req.params.u_id;
-    // var p_id = req.params.id;
-    // // find user in db by id
-    // User.findById(u_id, (err, foundUser) => {
-    //     //find profile embeded in user
-    //     var foundProfile = foundUser.touring.id(p_id)
-    //     //update the profile from the requested body
-    //     foundProfile.title = req.body.title;
-    //     //save 
-    //     foundProfile.save((err, savedUser) => {
-    //         res.json(foundProfile);
-    //     })
-    //     console.log("is good")
-    // })
+// set a new value of the user and profile 
+// var u_id = req.params.u_id;
+// var p_id = req.params.id;
+// // find user in db by id
+// User.findById(u_id, (err, foundUser) => {
+//     //find profile embeded in user
+//     var foundProfile = foundUser.touring.id(p_id)
+//     //update the profile from the requested body
+//     foundProfile.title = req.body.title;
+//     //save 
+//     foundProfile.save((err, savedUser) => {
+//         res.json(foundProfile);
+//     })
+//     console.log("is good")
+// })
 // })
 
 
 // edit - complete
-router.put('/api/user_edit/:id', (req, res)=>{
+router.put('/api/user_edit/:id', (req, res) => {
   // if the tour profile is not empty then make 
-  if(req.body.touring !== undefined)
-      {
-        req.body.tour = true;  
-  User.findOneAndUpdate({_id:req.params.id}, {$set:req.body}, {new: true} )
-    .then(userUpdate => {
-       
-    res.json(userUpdate)
-      }).catch(err =>
-      {
-        console.log("could not update tour user",err )
-      })
-    }else{
-  req.body.tour = false;  
-  User.findOneAndUpdate({_id:req.params.id}, {$set:req.body}, {new: true} )
-    .then(userUpdate => {
-       
-    res.json(userUpdate)
-      }).catch(err =>
-      {
-        console.log("could not update reg user",err )
-      })
-      
+  if (req.body.tourGuy !== undefined) {
+    req.body.tourType = "tourGuy";
+    User.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true })
+      .then(userUpdate => {
 
-    }
-  });
-    
-    //   else{
-    //     userUpdate.tour = false;
-    //     res.json(userUpdate);
-    //   }
-    // })  
-    //   // Catch any errors that might occur
-    //   .catch(function(err) {
-    //     res.status(status).json(obj);
-    //     //res.json("cant update", err);
-    //   });
-    // })
+        res.json(userUpdate)
+      }).catch(err => {
+        console.log("could not update tour user", err)
+      })
+  } else {
+    req.body.tour = false;
+    User.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true })
+      .then(userUpdate => {
 
-//Loging ---- Completed     
-router.post('/api/login', (req, res)=>{
-  //make sure they send pass & user
-  if(req.body.email && req.body.password){
-    User.findOne( {email: req.body.email},(err, user) => {
-          if (!user){
-            res.status(400).json({error: "Invalid pass or email"})
-          }
-           else {
-            if(req.body.email=== user.email && req.body.password=== user.password){
-                  const payLoad={id:user.id};
-                
-                //create token and send it to user 
-                const token = jwt.sign(payLoad,jwtOption.secretOrKey,{expiresIn:300})
-                res.status(200).json({success:true,token:token})
-              }
-              else{
-                res.status(401).json({error:'Invalid pass or email'})
-              }
-          }
-        }
-        )
+        res.json(userUpdate)
+      }).catch(err => {
+        console.log("could not update reg user", err)
+      })
+
 
   }
-  else{
-    res.status(400).json({error:'username & pass are required'})
+});
+
+
+//Loging ---- Completed     
+router.post('/api/login', (req, res) => {
+  //make sure they send pass & user
+  if (req.body.email && req.body.password) {
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (!user) {
+        res.status(400).json({ error: "Invalid pass or email" })
+      }
+      else {
+        if (req.body.email === user.email && req.body.password === user.password) {
+          const payLoad = { id: user.id };
+
+          //create token and send it to user 
+          const token = jwt.sign(payLoad, jwtOption.secretOrKey, { expiresIn: 300 })
+          res.status(200).json({ success: true, token: token })
+        }
+        else {
+          res.status(401).json({ error: 'Invalid pass or email' })
+        }
+      }
+    }
+    )
+
+  }
+  else {
+    res.status(400).json({ error: 'username & pass are required' })
   }
 })
 
-router.get('/api/protected', passport.authenticate('jwt',{session:false}), (req,res)=>{
-  res.json({message:'you are authorized',
-  user:req.user
-});
+router.get('/api/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    message: 'you are authorized',
+    user: req.user
+  });
 });
 
 // write a comment on tour guy profile
 router.post('/api/comment', (req, res) => {
   Comment.create({ comment: req.body.comment, id2: req.body.id2 })
-      .then(comment => {
-          Touring.findByIdAndUpdate(req.body.id, { $push: { comments: comment._id } })
-              .then(user => res.json({ msg: "the comment has been added " }))
-              .catch(err => res.sent(err))
-          res.send(comment)
-      })
-      .catch(err => {
-          console.log(err)
-          res.send(err)
-      })
+    .then(comment => {
+      console.log(comment)
+      console.log(User)
+      //  { $pash: { comments: comment._id }
+      User.findByIdAndUpdate(req.body.id, { $push: { comments: comment._id } })
+        .then(user => {
+          console.log(user)
+          res.json({ msg: "the comment has been added " })
+        })
+        .catch(err => res.sent(err))
+      // res.send(comment)
+    })
+    .catch(err => {
+      // console.log(err)
+      res.send(err)
+    })
 })
 
 
