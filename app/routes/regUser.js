7c8304f3f6a6fed2a46a83ef48aa4645b231e855
Hanin Nouh <haninnouh@gmail.com>
2020-02-05 01:51:43 +0300

    check cancel package

--- app/routes/regUser.js
+++ app/routes/regUser.js
@@ -15,9 +15,10 @@ const strategy = require('../lib/passportStrategy');
 passport.use(strategy);
 // Require Mongoose Model for User & Touring
 var RegUser = require('../models/regUser')
-var TourUser = require('../models/tourUser')
+var TourUser = require('../models/tourUser').TourUser
 var Booking = require('../models/booking');
 var Comment = require('../models/comment')
+var Package = require('../models/tourUser').Package
 
 router.use(express.urlencoded());
 
